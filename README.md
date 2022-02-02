f1893366f566f19ac982eec54d01f0e6011dd041
David Wang <Davidisathing@gmail.com>
2022-02-01 22:10:20 -0600

    Edits to README, added requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Usage
+# lehmler-lab-parser
+
+Installation
+------
+Usage of this script requires Python 3.9.7 or above.
+
+Clone this package and run `pip install -r requirements.txt` from the folder.
+
+Usage
+-----
 
 `python parse.py [PATH TO FILE]`
--- parse.py
+++ parse.py
@@ -1,23 +1,23 @@
 import pandas as pd
+from pathlib import Path
 import re
 import sys
 import os
 
 file = sys.argv[1]
 filename, file_extension = os.path.splitext(file)
-outname = os.path.join('./', filename + '.csv')
+outname = Path(file).stem
 
 read_file = pd.read_excel(file)
-  
-read_file.to_csv (filename + ".csv", 
+
+read_file.to_csv (filename + ".csv",
                   index = None,
                   header=True)
 
 df = pd.DataFrame(pd.read_csv(filename + ".csv"))
-
 df['Sample_ID'] = [re.sub('\+.*\)\s', '', str(x)) for x in df['Sample_ID']]
-
 df['Sample_ID'] = [re.sub('\s\s.*', '', str(x)) for x in df['Sample_ID']]
+df['Sample_ID'] = [re.sub('\s', '', str(x)) for x in df['Sample_ID']]
 
-df.to_csv(filename + ".csv", sep=',', encoding='utf-8', index=False)
-
+os.makedirs("./parsed", exist_ok=True)
+df.to_csv("./parsed/" + outname + ".csv", sep=',', encoding='utf-8', index=False)
